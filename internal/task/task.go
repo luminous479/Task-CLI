76de3720d7fc60f2b384b0d056f7f91138ca696f
luminous479 <aklemasultanakhi@gmail.com>
2026-09-06 20:03:28 +0600

    feat: add task creation and listing

--- internal/task/task.go
+++ internal/task/task.go
@@ -2,31 +2,36 @@ package task
 
 import "fmt"
 
-func Hello() {
-    fmt.Println("Hello from task package")
-}
-
 type Task struct{
 	ID int
 	Title string
 	Completed bool
 }
 
-
-func CreateTask(title string) Task {
-    return Task{
-        ID:        1,
-        Title:     title,
-        Completed: false,
-    }
-}
-
 func (t *Task) Complete() {
 	t.Completed = true 
 }
 
-func (t Task) PrintInfo(){
-	fmt.Println(t.ID)
-	fmt.Println(t.Title)
-	fmt.Println(t.Completed)
+
+type Manager struct {
+	tasks []Task
+} 
+
+func (m *Manager) AddTask(title string) Task{
+
+	task := Task{
+		ID: len(m.tasks) + 1,
+		Title: title,
+		Completed: false,
+	}
+	m.tasks = append(m.tasks, task)
+	return task
+} 
+
+func (m Manager) ListTasks() {
+	
+	for _, task := range m.tasks {
+		fmt.Println(task)
+	}		
+	
 }
--- main.go
+++ main.go
@@ -2,19 +2,45 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/luminous479/Task-CLI/internal/task"
 )
 
 func main() {
 	fmt.Println("Task CLI")
+	manager := task.Manager{}
 
-	task.Hello()
-	newTask := task.CreateTask("learn go")
-	fmt.Println(newTask.ID)
-	fmt.Println(newTask.Title)
-	fmt.Println(newTask.Completed)
-	newTask.Complete()
-	fmt.Println(newTask.Completed)
-    newTask.PrintInfo()
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: task <task-title>")
+		return
+	}
+
+	command := os.Args[1]
+
+	switch command {
+	case "add":
+
+		if len(os.Args) < 3 {
+			fmt.Println("Usage: task add <title>")
+			return
+		}
+
+		title := os.Args[2]
+		newTask := manager.AddTask(title)
+
+		fmt.Println("Task added:", newTask.Title)
+
+	case "list":
+		manager.ListTasks()
+
+	case "done":
+		fmt.Println("Complete Task")
+
+	case "delete":
+		fmt.Println("Delete Task")
+
+	default:
+		fmt.Println("Unknown command")
+	}
 }
