730f17ea6119cc3c015abd80b907cf7b586812cb
luminous479 <aklemasultanakhi@gmail.com>
2026-09-06 21:58:05 +0600

    refactor: separate CLI and task business logic

--- main.go
+++ main.go
@@ -9,72 +9,93 @@ import (
 )
 
 func main() {
-	fmt.Println("Task CLI")
 	manager := task.Manager{}
 
 	if len(os.Args) < 2 {
-		fmt.Println("Usage: task <task-title>")
+		fmt.Println("Usage:")
+		fmt.Println("  task add <title>")
+		fmt.Println("  task list")
+		fmt.Println("  task done <id>")
+		fmt.Println("  task delete <id>")
 		return
 	}
 
 	command := os.Args[1]
 
 	switch command {
-	case "add":
-		if len(os.Args) < 3 {
-			fmt.Println("Usage: task add <title>")
-			return
-		}
-		title := os.Args[2]
-		newTask := manager.AddTask(title)
 
-		fmt.Println("Task added:", newTask.Title)
+	case "add":
+		addTask(&manager)
 
 	case "list":
 		manager.ListTasks()
 
 	case "done":
-		if len(os.Args) < 3 {
-			fmt.Println("Usage: task done <task-id>")
-			return
-		}
-		taskID := os.Args[2]
-		id, err := strconv.Atoi(taskID)
-		if err != nil {
-			fmt.Println("Invalid task ID")
-			return
-		}
-		err = manager.CompleteTask(id)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		fmt.Println("Task marked as done")
+		completeTask(&manager)
 
 	case "delete":
-
-		if len(os.Args) < 3 {
-			fmt.Println("Usage: task delete <task-id>")
-			return
-		}
-
-		taskID := os.Args[2]
-
-		id, err := strconv.Atoi(taskID)
-		if err != nil {
-			fmt.Println("Invalid task ID")
-			return
-		}
-
-		err = manager.DeleteTask(id)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		fmt.Println("Task deleted")
+		deleteTask(&manager)
 
 	default:
-		fmt.Println("Unknown command")
+		fmt.Println("Unknown command:", command)
 	}
+}
+
+func addTask(manager *task.Manager) {
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: task add <title>")
+		return
+	}
+
+	title := os.Args[2]
+
+	newTask := manager.AddTask(title)
+
+	fmt.Println("Task added:", newTask.Title)
+}
+
+func completeTask(manager *task.Manager) {
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: task done <id>")
+		return
+	}
+
+	id, err := strconv.Atoi(os.Args[2])
+
+	if err != nil {
+		fmt.Println("Invalid task ID")
+		return
+	}
+
+	err = manager.CompleteTask(id)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Task marked as done")
+}
+
+func deleteTask(manager *task.Manager) {
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: task delete <id>")
+		return
+	}
+
+	id, err := strconv.Atoi(os.Args[2])
+
+	if err != nil {
+		fmt.Println("Invalid task ID")
+		return
+	}
+
+	err = manager.DeleteTask(id)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Task deleted")
 }
